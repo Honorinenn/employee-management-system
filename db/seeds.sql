2887920057dda2909c7973c13844a9d6672bf2da
Honorine Ndom <honorinendzah@gmail.com>
2021-11-10 04:21:17 -0800

    commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,19 +13,19 @@ VALUES
 INSERT INTO role
 (title, salary, department_id)
 VALUES
-('HR Manager', 100000, 1),
-('HR Associate', 55000, 1),
-('Accounting Manager', 110000, 2),
-('Accountant', 75000, 2),
-('Production Manager', 120000, 3),
-('Team Lead', 55000, 3),
-('Operator', 35000, 3),
-('Engineering Manager', 140000, 4),
-('Software Engineer', 100000, 4),
-('Lead Engineer', 120000, 4),
-('Sales Manager', 130000, 5),
-('Sales Lead', 95000, 5),
-('Salesperson', 75000, 5);
+('HR Manager', 120000, 1),
+('HR Associate', 75000, 1),
+('Accounting Manager', 130000, 2),
+('Accountant', 95000, 2),
+('Production Manager', 140000, 3),
+('Team Lead', 75000, 3),
+('Operator', 55000, 3),
+('Engineering Manager', 160000, 4),
+('Software Engineer', 120000, 4),
+('Lead Engineer', 140000, 4),
+('Sales Manager', 150000, 5),
+('Sales Lead', 115000, 5),
+('Salesperson', 95000, 5);
 -- SELECT * FROM role;
 
 INSERT INTO employee
